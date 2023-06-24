4ddff7c5783355fa1befc453788e3333c2479c19
Sanduranga <edirisinghest527@gmail.com>
2023-06-25 03:47:24 +0530

    applying middleware(logger&Thunk)

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import phoneReducer from "./redux/phoneSection/phoneReducer";
 import laptopReducer from "./redux/laptopSection/laptopReducer";
 import { buyPhone, sellPhone } from "./redux/phoneSection/phoneAction";
 import { buyLaptop, sellLaptop } from "./redux/laptopSection/laptopAction";
+import logger from "redux-logger";
 
 const rootReducer = combineReducers({
   phone: phoneReducer,
   laptop: laptopReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 
-store.subscribe(() => {
-  console.log(store.getState())
-})
+store.subscribe(() => {})
 
 store.dispatch(buyPhone())
 store.dispatch(sellLaptop())
